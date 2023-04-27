43539ca8bac9038df5fb0f4185f2c3bdf143d619
Nolan Kyhl <nkyhl@ND.EDU>
2023-04-27 18:51:39 -0400

    Model Loading & UI Expansion

--- Assets/Scripts/ScaleSlider.cs
+++ Assets/Scripts/ScaleSlider.cs
@@ -4,22 +4,23 @@ using Unity.VisualScripting;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.UI;
 
-public class ScaleSlider : MonoBehaviour
+public class ScaleSlider : MonoBehaviour 
 {
-    public GameObject referenceObject;
+    // This script controls the functionality of the ScaleSlider, and is applied to the ScaleManipulator helper object under NearMenu
     public GameObject slider;
+    public GameObject referenceObject;
     private float value;
     private Vector3 baseline;
 
     private void Awake() 
     {
+        // Set default values
         baseline = referenceObject.transform.localScale;
     }
 
     public void UpdateScale()
     {
-        if (referenceObject.activeSelf == false)
-            return;
+        if (referenceObject.activeSelf == false) return;
         
         value = slider.GetComponent<PinchSlider>().SliderValue * 2;
         if (value > 0.05)
@@ -29,7 +30,7 @@ public class ScaleSlider : MonoBehaviour
     public void ResetScale() 
     {
         referenceObject.transform.localScale = baseline;
-        slider.GetComponent<PinchSlider>().SliderValue = baseline.x / 2;
+        slider.GetComponent<PinchSlider>().SliderValue = 0.5f;
     }
 
 }
